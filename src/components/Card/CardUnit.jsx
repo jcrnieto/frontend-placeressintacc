1a4f76a0cace3584444b4d2bcf7892db38ca9473
jcrnieto <82995532+jcrnieto@users.noreply.github.com>
2023-02-06 17:35:42 -0300

    edicion imagen

--- src/components/Card/CardUnit.jsx
+++ src/components/Card/CardUnit.jsx
@@ -4,8 +4,8 @@ const Card = ({title, image, description, price}) => {
 
    
     return(
-           <div className="card rounded ms-3 mt-3 " style={{height: 400 , width: 16 +'rem'}}>
-             <img src={image} className="mx-auto mt-0" style={{ height: 250,width: 250}} alt="..."/>
+           <div className="card rounded ms-3 mt-3" style={{height: 400 , width: 16 +'rem'}}>
+             <img src={image} className="mx-auto mt-0 object-fit-cover" style={{ height: 250,width: 250}} alt="..."/>
                 <div className="card-body text-decoration-none">
                    <h5 className="card-title">{title}</h5>
                    <p className="card-text ">${price}</p>
